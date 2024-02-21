568eb404633871076a0349ef4bbe8de7dfae6adb
tvnhaaa <tvnhaaa@gmail.com>
2024-02-20 23:18:46 -0500

    changed some stuff

--- web/web_code/src/components/contact.jsx
+++ web/web_code/src/components/contact.jsx
@@ -130,19 +130,10 @@ export const Contact = (props) => {
               <div className="social">
                 <ul>
                   <li>
-                    <a href={props.data ? props.data.facebook : "/"}>
-                      <i className="fa fa-facebook"></i>
-                    </a>
                   </li>
                   <li>
-                    <a href={props.data ? props.data.twitter : "/"}>
-                      <i className="fa fa-twitter"></i>
-                    </a>
                   </li>
                   <li>
-                    <a href={props.data ? props.data.youtube : "/"}>
-                      <i className="fa fa-youtube"></i>
-                    </a>
                   </li>
                 </ul>
               </div>
@@ -152,12 +143,6 @@ export const Contact = (props) => {
       </div>
       <div id="footer">
         <div className="container text-center">
-          <p>
-            &copy; 2023 Issaaf Kattan React Land Page Template. Design by{" "}
-            <a href="http://www.templatewire.com" rel="nofollow">
-              TemplateWire
-            </a>
-          </p>
         </div>
       </div>
     </div>
