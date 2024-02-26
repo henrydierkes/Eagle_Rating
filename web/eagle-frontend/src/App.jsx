9376773fa5c44730e9313a9ad8d3d4022a799598
tvnhaaa <tvnhaaa@gmail.com>
2024-02-26 00:56:14 -0500

    hello

--- web/eagle-frontend/src/App.jsx
+++ web/eagle-frontend/src/App.jsx
@@ -1,6 +1,11 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import SmoothScroll from 'smooth-scroll';
+export const scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 1000,
+  speedAsDuration: true,
+});
+
 function App() {
   return (
     <Router>
