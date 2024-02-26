4402021cf47125b9ea01c44ee8eba6fe0bd3850d
tvnhaaa <tvnhaaa@gmail.com>
2024-02-26 01:58:46 -0500

    onion!

--- web/eagle-frontend/src/components/Background/Background.jsx
+++ web/eagle-frontend/src/components/Background/Background.jsx
@@ -6,7 +6,6 @@ const Background = () => {
     <div className="background-container">
       <h1 className="university-title">EMORY UNIVERSITY</h1>
       <button className="choose-campus-btn">Choose Campus</button>
-      <div className="university-address">201 Dowman Dr, Atlanta, GA</div>
     </div>
   );
 };
