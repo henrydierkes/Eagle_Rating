7757fba9807a1c3d706881e7e2a5945fff6a66ff
henrydierkes <henry.dierkes@emory.edu>
2024-02-25 23:56:32 -0500

    Beginning of Navigation page

---
+++ web/eagle-frontend/src/pages/Navigation/Navigation.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import NavBar from "../../components/NavBar/NavBar";
+import Background from "../../components/Background/Background";
+import Category from "../../components/Category/Category";
+import SearchBar from "../../components/SearchBar/SearchBar";
+import TopComments from "../../components/TopComments/TopComments";
+
+import "./Navitagion.css";
+function Navitagion() {
+  return (
+    <div className="Navigation">
+      <NavBar />
+      <Background />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
