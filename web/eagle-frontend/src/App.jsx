af4dce42b8131419570b628c46aeb073595e0407
tvnhaaa <tvnhaaa@gmail.com>
2024-02-26 00:43:30 -0500

    fix smooth scrolling and other things

--- web/eagle-frontend/src/App.jsx
+++ web/eagle-frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home/Home";
-
+import SmoothScroll from 'smooth-scroll';
 function App() {
   return (
     <Router>
