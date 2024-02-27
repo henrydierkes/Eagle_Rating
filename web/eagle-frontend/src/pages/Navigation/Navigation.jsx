17aacf1ccdfe1fafff85a303a9385ca7b924c03d
mawenzhuo2022 <mawenzhuo2022@gmail.com>
2024-02-28 04:46:38 +0800

    add components to Navigation.jsx

--- web/eagle-frontend/src/pages/Navigation/Navigation.jsx
+++ web/eagle-frontend/src/pages/Navigation/Navigation.jsx
@@ -4,9 +4,9 @@ import ResultList from "../../components/ResultList/ResultList";
 import Footer from "../../components/Footer/Footer";
 
 const results = [
-  { id: 1, title: "Result 1", description: "Description for Result 1" },
-  { id: 2, title: "Result 2", description: "Description for Result 2" },
-  { id: 3, title: "Result 3", description: "Description for Result 3" },
+  { id: 1, title: "Result 1", description: "Description for Result 1", rating: 4.5 },
+  { id: 2, title: "Result 2", description: "Description for Result 2", rating: 3.2 },
+  { id: 3, title: "Result 3", description: "Description for Result 3", rating: 2.4 },
 ];
 
 import "./Navigation.css";
