d9c43e159aa78413f11939f4b81868c5a39ac594
henrydierkes <henry.dierkes@emory.edu>
2024-02-28 00:09:21 -0500

    small update

--- web/eagle-frontend/src/components/subComponents/ResultsAndFilter/ResultsAndFilter.jsx
+++ web/eagle-frontend/src/components/subComponents/ResultsAndFilter/ResultsAndFilter.jsx
@@ -7,9 +7,6 @@ const ResultsAndFilter = ( {results} ) => {
   const [filteredResults, setFilteredResults] = useState(results);
 
   const handleFilterChange = (filters) => {
-    // Apply filters to results and update filteredResults state
-    // Here you would implement your filtering logic based on the received filters
-    // For now, let's just console.log the filters
     console.log('Filters applied:', filters);
   };
 
