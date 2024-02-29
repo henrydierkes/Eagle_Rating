2f0fad1b40c8e7e39a2451736db9e5cb2502cb34
henrydierkes <henry.dierkes@emory.edu>
2024-02-29 14:41:37 -0500

    Adjusted spacing on footer

--- web/eagle-frontend/src/pages/RatingPage/RatingPage.jsx
+++ web/eagle-frontend/src/pages/RatingPage/RatingPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import NavBar from "../../components/NavBar/NavBar.jsx";
-import ResultsAndFilter from "../../components/subComponents/ResultsAndFilter/ResultsAndFilter.jsx"
+
 import Footer from "../../components/Footer/Footer.jsx";
 
 
