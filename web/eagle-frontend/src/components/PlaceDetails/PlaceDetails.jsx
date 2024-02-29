bc64b77a9deb09300f21564524b7f92e4b2d0904
henrydierkes <henry.dierkes@emory.edu>
2024-02-29 17:19:59 -0500

    Managed to get the number displayed on the rating details page

--- web/eagle-frontend/src/components/PlaceDetails/PlaceDetails.jsx
+++ web/eagle-frontend/src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './PlaceDetails.css';
 
-const PlaceDetails = () => {
+const getRatingColor = (rating) => {
+  if (rating >= 4) {
+      return '#4CAF50'; // green
+  } else if (rating >= 3) {
+      return '#CDDC39'; // lime
+  } else if (rating >= 2) {
+      return '#FFC107'; // amber
+  } else {
+      return '#F44336'; // red
+  }
+};
+
+const PlaceDetails = ({ results }) => {
+  const result=results[0]; // This is very important because results.rating can't read a whole array, it needs to read an item in the array
   return (
     <div className="place-details">
+      <div
+          className="rating-box-details"
+          style={{ backgroundColor: getRatingColor(result.rating) }}
+          >
+            <span className="rating-number-details">{result.rating}</span>
+      </div>
     </div>
   );
 };
--- web/eagle-frontend/src/pages/RatingPage/RatingPage.jsx
+++ web/eagle-frontend/src/pages/RatingPage/RatingPage.jsx
@@ -4,18 +4,18 @@ import PlaceDetails from "../../components/PlaceDetails/PlaceDetails.jsx"
 import CommentFilter from "../../components/CommentFilter/CommentFilter.jsx"
 import CommentList from "../../components/CommentList/CommentList.jsx"
 import Footer from "../../components/Footer/Footer.jsx";
+import "./RatingPage.css"
 
 
 const results = [
     { id: 1, title: "Result 1", description: "Description for Result 1", rating: 4.5, num_rate: 33 },
-    { id: 2, title: "Result 2", description: "Description for Result 2", rating: 3.2, num_rate: 20 },
-    { id: 3, title: "Result 3", description: "Description for Result 3", rating: 2.4, num_rate: 5 },
 ];
+
 function RatingPage() {
     return (
         <div className="RatingPage">
             <NavBar />
-            <PlaceDetails />
+            <PlaceDetails results={results}/>
             <CommentFilter />
             <CommentList/>
             <Footer />
