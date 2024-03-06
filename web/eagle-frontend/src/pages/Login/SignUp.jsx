ffe4f553d5e31f2107e2cd07fb4cf94a16a65f84
tvnhaaa <tvnhaaa@gmail.com>
2024-03-06 11:49:26 -0500

    Revert "changes"
    This reverts commit b86ed14639223fb6a56d22e56de0004f67c5a15c.

--- web/eagle-frontend/src/pages/Login/SignUp.jsx
+++ web/eagle-frontend/src/pages/Login/SignUp.jsx
@@ -39,7 +39,7 @@ export default function SignUp() {
           }}
         >
           <Typography component="h1" variant="h5">
-            Log In
+            Sign up
           </Typography>
           <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
             <Grid container spacing={2}>
