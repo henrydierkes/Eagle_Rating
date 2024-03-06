2e4d4c555da4fcebe9197c2ddc486e2f2a78b18b
mawenzhuo2022 <mawenzhuo2022@gmail.com>
2024-03-07 06:24:27 +0800

    rating bar

--- web/eagle-frontend/src/pages/RatingPage/RatingPage.jsx
+++ web/eagle-frontend/src/pages/RatingPage/RatingPage.jsx
@@ -5,10 +5,26 @@ import CommentFilter from "../../components/CommentFilter/CommentFilter.jsx"
 import CommentList from "../../components/CommentList/CommentList.jsx"
 import Footer from "../../components/Footer/Footer.jsx";
 import "./RatingPage.css"
+import DetailRating from "../../components/subComponents/detailRating/DetailRating.jsx";
 
 
 const results = [
-    { id: 1, title: "Place 567", description: "Description for Result 1", rating: 4.5, num_rate: 33, location: '1400 Emory Street', top_tags: ['tables', 'chairs'], size: 5, clean: 5, quiet: 5, building: 'Building A', floor: '2nd floor'},
+    { id: 1, title: "Place 567", description: "Description for Result 1", rating: 4.5, num_rate: 33, location: '1400 Emory Street', top_tags: ['tables', 'chairs'],
+        details: {
+            cleanliness: {
+                name: "Cleanliness",
+                rating: 4.5,
+            },
+            service: {
+                name: "Service",
+                rating: 4.0,
+            },
+            location: {
+                name: "Location",
+                rating: 5.0,
+            },
+        },
+        building: 'Building A', floor: '2nd floor'},
 ];
 
 function RatingPage() {
@@ -16,6 +32,7 @@ function RatingPage() {
         <div className="RatingPage">
             <NavBar />
             <PlaceDetails results={results}/>
+            <DetailRating results={results}/>
             <CommentFilter />
             <CommentList/>
             <Footer />
