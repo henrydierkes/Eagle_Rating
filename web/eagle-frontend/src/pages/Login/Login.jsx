546b97edf167007b544ab5b42aa98f5c5f9a1573
henrydierkes <henry.dierkes@emory.edu>
2024-03-05 21:49:40 -0500

    Added login files

---
+++ web/eagle-frontend/src/pages/Login/Login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./Login.css"
+
+function Login() {
+    return (
+        <div className="Login">
+            
+        </div>
+    );
+}
+
+
+export default Login;
