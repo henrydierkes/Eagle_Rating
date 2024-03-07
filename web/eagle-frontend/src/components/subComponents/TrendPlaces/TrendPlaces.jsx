38c82c93de0809348945e4929880a13edaac6b11
Jonathan Wang <112515982+JonathanWry@users.noreply.github.com>
2024-03-06 22:17:18 -0500

    tried to use slick but my computer seems to have version conflict

--- web/eagle-frontend/src/components/subComponents/TrendPlaces/TrendPlaces.jsx
+++ web/eagle-frontend/src/components/subComponents/TrendPlaces/TrendPlaces.jsx
@@ -1,36 +1,65 @@
-import React, {useRef} from "react";
+import React, { useEffect, useRef } from "react";
 import "./TrendPlaces.css";
 import TrendPlace from "../TrendPlace/TrendPlace";
+import $ from "jquery"; // Import jQuery if not already done
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel';
 
 function TrendPlaces() {
     const slideRef = useRef(null);
+    const slickRef = useRef(null);
+
+    useEffect(() => {
+        const slideElement = slideRef.current;
+        if (slideElement) {
+            const slickInstance = $(slideElement).slick({
+                dots: false,
+                arrows: true,
+                slidesToShow: 5,
+                infinite: false,
+            });
+
+            slickRef.current = slickInstance;
+
+            return () => {
+                if (slickInstance) {
+                    slickInstance.slick('unslick');
+                }
+            };
+        }
+    }, [slideRef.current]);
+
 
     const scrollRight = () => {
         if (slideRef.current) {
-            let lists = slideRef.current.querySelectorAll('.TrendPlace');
-            slideRef.current.appendChild(lists[0]);
+            jQuery(slideRef.current).slick("slickNext");
         }
     };
 
     const scrollLeft = () => {
         if (slideRef.current) {
-            let lists = slideRef.current.querySelectorAll('.TrendPlace');
-            slideRef.current.prepend(lists[lists.length - 1]);
+            jQuery(slideRef.current).slick("slickPrev");
         }
     };
+
     return (
-            <div className="TrendPlaces">
-                <div className="buttons">
-                    <button id="prev" onClick={scrollLeft}><i className="fas fa-angle-left" style={{ fontSize: '24px' }}></i></button>
-                    <button id="next" onClick={scrollRight}><i className="fas fa-angle-right" style={{ fontSize: '24px' }}></i></button>
-                </div>
-                <div className="slide" ref={slideRef}>
-                    <TrendPlace name="Place 1" />
-                    <TrendPlace name="Place 2" />
-                    <TrendPlace name="Place 3" />
-                    <TrendPlace name="Place 4" />
-                    <TrendPlace name="Place 5" />
-                </div>
+        <div className="TrendPlaces">
+            <div className="buttons">
+                <button id="prev" onClick={scrollLeft}>
+                    <i className="fas fa-angle-left" style={{ fontSize: "24px" }}></i>
+                </button>
+                <button id="next" onClick={scrollRight}>
+                    <i className="fas fa-angle-right" style={{ fontSize: "24px" }}></i>
+                </button>
+            </div>
+            <div className="slide" ref={slideRef}>
+                <TrendPlace name="Place 1" />
+                <TrendPlace name="Place 2" />
+                <TrendPlace name="Place 3" />
+                <TrendPlace name="Place 4" />
+                <TrendPlace name="Place 5" />
+            </div>
         </div>
     );
 }
