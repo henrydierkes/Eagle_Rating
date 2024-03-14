8e345f5c89375a8d46503389aba5cd3543b0199c
Jonathan Wang <112515982+JonathanWry@users.noreply.github.com>
2024-03-14 00:36:50 -0400

    Transaction in deleteRating and addRating

--- main/RatingBackend/src/main/java/com/astar/ratingbackend/Controller/RatingController.java
+++ main/RatingBackend/src/main/java/com/astar/ratingbackend/Controller/RatingController.java
@@ -1,13 +1,10 @@
 package com.astar.ratingbackend.Controller;
 
 import com.astar.ratingbackend.Entity.CommentFilterRequest;
-import com.astar.ratingbackend.Entity.Place;
 import com.astar.ratingbackend.Entity.Rating;
-import com.astar.ratingbackend.Entity.User;
 import com.astar.ratingbackend.Service.IPlaceService;
 import com.astar.ratingbackend.Service.IRatingService;
 import com.astar.ratingbackend.Service.IUserService;
-import com.mongodb.client.ClientSession;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
@@ -70,24 +67,9 @@ public class RatingController {
      * @return A ResponseEntity indicating the outcome of the operation.
      */
     @DeleteMapping("/delete")
-    public ResponseEntity<Object> deleteRatingById(@RequestParam String ratingId) {
-        try {
-            Rating rating=ratingService.validateRating(ratingId);
-            User user = userService.validateUser(rating.getUserId());
-            Place place = placeService.validatePlace(rating.getPlaceId());
-            boolean ratingDeleted = ratingService.deleteRating(ratingId);
-            boolean userDeleted = userService.deleteRating(rating);
-            boolean placeDeleted = placeService.deleteRating(rating);
-            if (!ratingDeleted || !userDeleted || !placeDeleted) {
-                // If any deletion fails, return an INTERNAL_SERVER_ERROR response
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-            }
-            // If deletion is successful, return an OK response
-            return ResponseEntity.ok().build();
-        } catch (IllegalArgumentException e) {
-            // If an IllegalArgumentException occurs, return an INTERNAL_SERVER_ERROR response
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-        }
+    public ResponseEntity<String> deleteRatingById(@RequestParam String ratingId) {
+        return ratingService.deleteRating(ratingId);
+
     }
 
     /**
@@ -96,19 +78,8 @@ public class RatingController {
      * @return A ResponseEntity containing the created rating or an error status.
      */
     @PostMapping("/addRating")
-    public ResponseEntity<Place> addRating(@RequestBody Rating rating) {
-        String placeId = rating.getPlaceId();
-        try {
-            ClientSession session = mongoDatabaseFactory.getSession();
-            User user = userService.validateUser(rating.getUserId());
-            Place place = placeService.validatePlace(placeId);
-            Rating addedRating = ratingService.addRating(rating, user);
-            userService.addRating(addedRating);
-            ResponseEntity<Place> response = placeService.addRating(placeId, addedRating);
-            return ResponseEntity.ok(response.getBody());
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-        }
+    public ResponseEntity<String> addRating(@RequestBody Rating rating) {
+        return ratingService.addRating(rating);
     }
 
     /**
