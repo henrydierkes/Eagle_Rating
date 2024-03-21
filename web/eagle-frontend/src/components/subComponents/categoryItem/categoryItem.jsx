0590d96ee1fee9efea03f74409018406e25d0e1f
henrydierkes <henry.dierkes@emory.edu>
2024-03-20 21:57:56 -0400

    change

--- web/eagle-frontend/src/components/subComponents/categoryItem/categoryItem.jsx
+++ web/eagle-frontend/src/components/subComponents/categoryItem/categoryItem.jsx
@@ -11,8 +11,23 @@ function CategoryItem({ name, imageUrl }) {
     borderRadius: "10px", // Apply border radius to the background
   };
 
+  const handleClick = () => {
+    axios.get("http://localhost:8080/api/place/search", {
+      params: {
+        category: name
+      }
+    })
+    .then(response => {
+      // Handle the response data here
+      console.log("Response data:", response.data);
+    })
+    .catch(error => {
+      console.error("Error fetching data:", error);
+    });
+  };
+
   return (
-    <div className="CategoryItem" style={backgroundStyle}>
+    <div className="CategoryItem" style={backgroundStyle} onClick={handleClick}>
       <div className="Name">
         <h2>{name}</h2>
       </div>
