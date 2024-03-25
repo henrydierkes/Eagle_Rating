dbf70e7f055e9ef93b1dc1fd1fd739a82ef2dece
henrydierkes <henry.dierkes@emory.edu>
2024-03-24 23:31:09 -0400

    implemented going to navigation page when hitting enter on search bar

--- web/eagle-frontend/src/components/SearchBar/SearchBar.jsx
+++ web/eagle-frontend/src/components/SearchBar/SearchBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
 import InputAdornment from '@mui/material/InputAdornment';
@@ -9,13 +9,22 @@ const placesInEmory = [
   { title: 'Woodruff Library' },
   { title: 'Cox Hall' },
   { title: 'Emory Student Center' },
-  { title: 'Mathematics and Science Center'},
-  { title: 'Atwood Chemistry Center'},
-  { title: 'White Hall'},
-  { title: 'Harris Hall'}
+  { title: 'Mathematics and Science Center' },
+  { title: 'Atwood Chemistry Center' },
+  { title: 'White Hall' },
+  { title: 'Harris Hall' }
 ];
 
 const SearchBar = () => {
+  const [selectedTags, setSelectedTags] = useState([]);
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      const encodedTags = encodeURIComponent(selectedTags.map(tag => tag.title).join(','));
+      window.location.href = `/navigation?tags=${encodedTags}`;
+    }
+  };
+
   return (
     <div className="search-bar" style={{ display: 'flex', width: '100%', justifyContent: 'flex-start' }}>
       <Autocomplete
@@ -24,11 +33,15 @@ const SearchBar = () => {
         id="tags-limit-search"
         options={placesInEmory}
         getOptionLabel={(option) => option.title}
+        onChange={(event, newValue) => {
+          setSelectedTags(newValue); // Update selectedTags with the selected values
+        }}
         renderInput={(params) => (
           <TextField
             {...params}
             variant="outlined"
-            placeholder="Starting typing to look for places!"
+            placeholder="Start typing to look for places!"
+            onKeyDown={handleKeyDown}
           />
         )}
         sx={{ width: '100%', height: '80%', marginLeft: '20px', marginRight: '20px' }}
