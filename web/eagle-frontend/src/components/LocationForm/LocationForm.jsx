4e2fa7e9f499b3a4a2e96263a5f3e5ecc1a2acc5
mawenzhuo2022 <mawenzhuo2022@gmail.com>
2024-03-26 09:14:07 +0800

    AddLocation determine add rating

--- web/eagle-frontend/src/components/LocationForm/LocationForm.jsx
+++ web/eagle-frontend/src/components/LocationForm/LocationForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Rating from '@mui/material/Rating';
+import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import InputLabel from '@mui/material/InputLabel';
@@ -10,7 +11,10 @@ import ListItemText from '@mui/material/ListItemText';
 import Select from '@mui/material/Select';
 import Checkbox from '@mui/material/Checkbox';
 import './LocationForm.css';
-import axios from 'axios'; // Import Axios
+import axios from 'axios';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+
 
 
 const ITEM_HEIGHT = 48;
@@ -32,6 +36,12 @@ const tags = [
   'Sussy imposter'
 ];
 
+
+
+const toggleRatingVisibility = () => {
+  setIsRatingVisible(!isRatingVisible);
+};
+
 const LocationForm = () => {
   const [formData, setFormData] = useState({
     placeName: '',
@@ -47,8 +57,15 @@ const LocationForm = () => {
     uploadedImages: []
   });
 
+  // 新增的状态和设置这个状态的函数
+  const [isRatingVisible, setIsRatingVisible] = useState(false);
+
+  const [ratingType, setRatingType] = useState("total"); // 初始值为 "total"
+
+
+
   const handleInputChange = (e) => {
-    const { name, value } = e.target;
+    const {name, value} = e.target;
     setFormData({
       ...formData,
       [name]: value
@@ -100,7 +117,7 @@ const LocationForm = () => {
       }, {}),
       ratingCount: 0, // Assuming there's always at least 1 rating for a new place
       ratingIds: [],
-      images: formData.uploadedImages.map(image => ({ data: image })),
+      images: formData.uploadedImages.map(image => ({data: image})),
       // totalRating: {
       //   overall: formData.rating,
       //   rating1: formData.size,
@@ -176,87 +193,120 @@ const LocationForm = () => {
             onChange={handleInputChange}
         />
 
-        <form onSubmit={handleSubmit}>
-          <div className='overall-rating'>
-            <Typography component="legend">Overall Rating:</Typography>
-            <Rating
-                name="rating"
-                value={formData.rating}
-                onChange={(event, newValue) => handleRatingChange('rating', newValue)}
-            />
-          </div>
-          <div className='size'>
-            <Typography component="legend">Size:</Typography>
-            <Rating
-                name="size"
-                value={formData.size}
-                onChange={(event, newValue) => handleRatingChange('size', newValue)}
-            />
-          </div>
-          <div className='cleanliness'>
-            <Typography component="legend">Cleanliness:</Typography>
-            <Rating
-                name="cleanliness"
-                value={formData.cleanliness}
-                onChange={(event, newValue) => handleRatingChange('cleanliness', newValue)}
-            />
-          </div>
-          <div className='quietness'>
-            <Typography component="legend">Quietness:</Typography>
-            <Rating
-                name="quietness"
-                value={formData.quietness}
-                onChange={(event, newValue) => handleRatingChange('quietness', newValue)}
-            />
-          </div>
-          <div>
-            <div className='rating-tags'>
-              <FormControl sx={{ m: 1, width: 'auto', minWidth: 200, maxWidth: 450}}>
-                <InputLabel id="tags-label">Tags</InputLabel>
-                <Select
-                    labelId="tags-label"
-                    id="tags-select"
-                    multiple
-                    value={formData.tags}
-                    onChange={handleTagsChange}
-                    input={<OutlinedInput label="Tag" />}
-                    renderValue={(selected) => selected.join(', ')}
-                    MenuProps={MenuProps}
-                >
-                  {tags.map((name) => (
-                      <MenuItem key={name} value={name}>
-                        <Checkbox checked={formData.tags.indexOf(name) > -1} />
-                        <ListItemText primary={name} />
-                      </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-            </div>
-          </div>
-          <div className='comment-rating'>
-            <TextField
-                id="filled-textarea"
-                label="Comment"
-                placeholder="Type comment here"
-                multiline
-                variant="filled"
-                name="comment"
-                value={formData.comment}
-                onChange={handleInputChange}
-            />
-          </div>
-          <div className="upload-images">
-            <label htmlFor="upload" className="upload-label">
-              <span>Upload Image</span>
-              <input id="upload" type="file" multiple onChange={handleImageChange} className="upload-input" />
-            </label>
-            {formData.uploadedImages.map((image, index) => (
-                <img className='uploaded-image'key={index} src={image} alt={`Uploaded Image ${index + 1}`} />
-            ))}
-          </div>
 
-          <button className='submit-button' type="submit">Submit</button>
-        </form>
+        {/* Toggle Button for Ratings Visibility */}
+        <Button onClick={() => setIsRatingVisible(!isRatingVisible)} style={{marginBottom: '1rem'}}>
+          Add Rating
+        </Button>
+
+
+
+        {/* Conditional Rendering based on isRatingVisible state */}
+        {isRatingVisible && (
+            <>
+              <ToggleButtonGroup
+                  value={ratingType}
+                  exclusive
+                  onChange={(event, newRatingType) => setRatingType(newRatingType)}
+                  aria-label="rating type"
+                  sx={{ mb: 1 }}
+              >
+                <ToggleButton value="total" aria-label="left aligned">
+                  Total Rating
+                </ToggleButton>
+                <ToggleButton value="sub" aria-label="centered">
+                  Subrating
+                </ToggleButton>
+              </ToggleButtonGroup>
+
+
+              {ratingType === 'total' && (
+                  <div className='overall-rating'>
+                    <Typography component="legend">Overall Rating:</Typography>
+                    <Rating
+                        name="rating"
+                        value={formData.rating}
+                        onChange={(event, newValue) => handleRatingChange('rating', newValue)}
+                    />
+                  </div>
+              )}
+              {ratingType === 'sub' && (
+                  <>
+                  <div className='size'>
+                    <Typography component="legend">Size:</Typography>
+                    <Rating
+                        name="size"
+                        value={formData.size}
+                        onChange={(event, newValue) => handleRatingChange('size', newValue)}
+                    />
+                  </div>
+                  <div className='cleanliness'>
+                    <Typography component="legend">Cleanliness:</Typography>
+                    <Rating
+                        name="cleanliness"
+                        value={formData.cleanliness}
+                        onChange={(event, newValue) => handleRatingChange('cleanliness', newValue)}
+                    />
+                  </div>
+                  <div className='quietness'>
+                    <Typography component="legend">Quietness:</Typography>
+                    <Rating
+                        name="quietness"
+                        value={formData.quietness}
+                        onChange={(event, newValue) => handleRatingChange('quietness', newValue)}
+                    />
+                  </div>
+                  </>
+                )}
+            </>
+              )}
+
+        <div>
+          <div className='rating-tags'>
+            <FormControl sx={{ m: 1, width: 'auto', minWidth: 200, maxWidth: 450}}>
+              <InputLabel id="tags-label">Tags</InputLabel>
+              <Select
+                  labelId="tags-label"
+                  id="tags-select"
+                  multiple
+                  value={formData.tags}
+                  onChange={handleTagsChange}
+                  input={<OutlinedInput label="Tag" />}
+                  renderValue={(selected) => selected.join(', ')}
+                  MenuProps={MenuProps}
+              >
+                {tags.map((name) => (
+                    <MenuItem key={name} value={name}>
+                      <Checkbox checked={formData.tags.indexOf(name) > -1} />
+                      <ListItemText primary={name} />
+                    </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </div>
+        </div>
+        <div className='comment-rating'>
+          <TextField
+              id="filled-textarea"
+              label="Comment"
+              placeholder="Type comment here"
+              multiline
+              variant="filled"
+              name="comment"
+              value={formData.comment}
+              onChange={handleInputChange}
+          />
+        </div>
+        <div className="upload-images">
+          <label htmlFor="upload" className="upload-label">
+            <span>Upload Image</span>
+            <input id="upload" type="file" multiple onChange={handleImageChange} className="upload-input" />
+          </label>
+          {formData.uploadedImages.map((image, index) => (
+              <img className='uploaded-image'key={index} src={image} alt={`Uploaded Image ${index + 1}`} />
+          ))}
+        </div>
+        <button className='submit-button' type="submit">Submit</button>
       </div>
   );
 };
