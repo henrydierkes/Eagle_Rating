a0ade4880a2426ce65eaa6fce30511a2c1a9b64c
Jonathan Wang <112515982+JonathanWry@users.noreply.github.com>
2024-04-06 21:48:28 -0400

    udpate

--- web/eagle-frontend/src/axiosConfig.jsx
+++ web/eagle-frontend/src/axiosConfig.jsx
@@ -1,7 +1,7 @@
 // config.js
 const axiosConfig = {
-    // baseURL: 'http://localhost:8080' // Replace this with your backend server URL
-    baseURL: 'http://3.134.76.90:8080'
+    baseURL: 'http://localhost:8080' // Replace this with your backend server URL
+    // baseURL: 'http://3.134.76.90:8080'
 
 };
 
