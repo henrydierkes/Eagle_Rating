59b32937a44aecd4d07e3b528e144b65da0470cd
henrydierkes <henry.dierkes@emory.edu>
2024-04-30 18:11:01 -0400

    fixed visibility

--- main/RatingBackend/src/main/java/com/astar/ratingbackend/RatingBackendApplication.java
+++ main/RatingBackend/src/main/java/com/astar/ratingbackend/RatingBackendApplication.java
@@ -6,9 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 
 @SpringBootApplication
+@PropertySources({@PropertySource("classpath:application.yml"), @PropertySource("file:${user.dir}/.env")})
 public class RatingBackendApplication {
 
     public static void main(String[] args) {
